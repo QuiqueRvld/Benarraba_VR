f8a38423b19a9b06e77fc5a1b33322b01c5cea1d
Franmuci <franloko70@gmail.com>
2024-12-16 12:46:03 +0100

    Prueba conflicto 2.2
    Fallo mio en la anterior prueba

--- Assets/Scripts/DestructionObject.cs
+++ Assets/Scripts/DestructionObject.cs
@@ -4,7 +4,7 @@ public class DestructionObject : MonoBehaviour
 {
     public GameObject destroyedVersion;
     
-    private void OnMouseUp()
+    private void OnMouse()
     {
        
         Instantiate (destroyedVersion, transform.position, transform.rotation);
